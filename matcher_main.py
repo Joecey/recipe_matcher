7cad6286cab0e9d2c7fbe26d528d96f3cfa83094
Joe Linogao <linogaoj@tcd.ie>
2022-02-16 18:55:53 +0000

    started main code

--- matcher_main.py
+++ matcher_main.py
@@ -1,1 +1,21 @@
 # recipe matcher code (using breakfast api)
+
+# functions
+def convert(lst):
+    converted = lst.split()
+    return converted
+
+# find suitable recipe
+def find_recipe():
+    pass
+
+# ask user for food list
+ask_food = str(input("Please list food available in pantry. Use lower case letter and separate items with a space. \n"))
+
+# convert string into seperate words
+food_list = convert(ask_food)
+
+# print food list
+for i in food_list: print(i)
+
+
