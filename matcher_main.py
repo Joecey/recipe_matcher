379b5ac76173fcec8068e999507d9c3d7d7fe508
Joe Linogao <linogaoj@tcd.ie>
2022-02-22 19:55:15 +0000

    changed method of adding food

--- matcher_main.py
+++ matcher_main.py
@@ -1,21 +1,43 @@
 # recipe matcher code (using breakfast api)
+# functions (list all ingredients in one input)
 
-# functions
 def convert(lst):
     converted = lst.split()
     return converted
 
-# find suitable recipe
-def find_recipe():
+# list ingredients one by one
+def give_ingredients():
+    repeat_ask = True
+    food_list = []
+
+    while repeat_ask is True:
+        ask = str(input("Type food item in lower case, or type DONE to complete: "))
+        
+        if ask == "DONE":
+            repeat_ask = False
+
+        else:
+            # add a checker for miss spellings? (compare to ingredients.txt file???)
+            # add ingredient to food list
+            food_list.append(ask)
+
+    return food_list
+
+# find suitable recipe with food list
+def find_recipe(food_list):
     pass
 
+
 # ask user for food list
-ask_food = str(input("Please list food available in pantry. Use lower case letter and separate items with a space. \n"))
+# ask_food = str(input("Please list food available in pantry. Use lower case letter and separate items with a space. \n"))
 
-# convert string into seperate words
-food_list = convert(ask_food)
+food_list = give_ingredients()
 
-# print food list
-for i in food_list: print(i)
 
+# depending on what's in food list...
+if len(food_list) == 0:
+    print("food list is empty")
+
+else:
+    print(food_list)
 
